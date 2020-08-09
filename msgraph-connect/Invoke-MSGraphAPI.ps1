cf45c3a2dbb9b2fd825ebb1593e2e4dd3a8d3a90
Adam Edwards <adamedx@gmail.com>
2020-08-09 07:34:30 -0700

    Updates to powershell script

--- msgraph-connect/Invoke-MSGraphAPI.ps1
+++ msgraph-connect/Invoke-MSGraphAPI.ps1
@@ -1,3 +1,5 @@
+using assembly ".\msgraph-connect.dll"
+
 # Copyright 2020, Adam Edwards
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,16 +14,93 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-using assembly ".\msgraph-connect.dll"
+set-strictmode -version 2
 
 function Invoke-MSGraphAPI {
-    [cmdletbinding()]
-    param()
+    [cmdletbinding(positionalbinding=$false)]
+    param(
+        [parameter(position=0, mandatory=$true)]
+        [string] $Uri,
+
+        [string] $Method = 'GET',
+
+        [parameter(parametersetname='simple')]
+        [string] $Version = '1.0',
+
+        [parameter(parametersetname='simple')]
+        [Uri] $GraphUri = 'https://graph.microsoft.com',
+
+        [parameter(parametersetname='simple')]
+        [Uri] $LoginUri = 'https://login.microsoftonline.com',
+
+        [parameter(parametersetname='simple')]
+        [string[]] $Permissions = @('User.Read'),
+
+        [parameter(parametersetname='simple')]
+        [string] $AppId,
+
+        [switch] $FullResponse,
+
+        [switch] $RawContent,
+
+        [parameter(parametersetname='connection', mandatory=$true)]
+        [msgraph_connect.GraphConnection] $Connection
+    )
+
+    $targetAppId = if ( $AppId ) {
+        [Guid] $AppId
+    }
+
+    $graphConnection = if ( $Connection ) {
+        $Connection
+    } else {
+        [msgraph_connect.GraphConnection]::new($Permissions, $GraphUri, $LoginUri, $Version, $targetAppId)
+    }
+
+    write-host $uri, $method
+
+    $graphConnection | fl * | out-host
+    $response = $graphConnection.InvokeRequest($Uri, $Method)
+
+    $global:mycon = $graphConnection
+    if ( $FullResponse.IsPresent ) {
+        $response
+    } else {
+        $content = $response.Content
+        if ( $RawContent.IsPresent ) {
+            $content
+        } else {
+            $content | convertfrom-json
+        }
+    }
 }
 
 function Connect-MSGraphAPI {
-    [cmdletbinding()]
-    param()
+    [cmdletbinding(positionalbinding=0)]
+    param(
+        [string] $Version,
 
-    [msgraph_connect.GraphConnection]
+        [Uri] $GraphUri = 'https://graph.microsoft.com',
+
+        [Uri] $LoginUri = 'https://login.microsoftonline.com',
+
+        [string[]] $Permissions = @('User.Read'),
+
+        [string] $AppId,
+
+        [switch] $NoConnect
+    )
+
+    $connection = [msgraph_connect.GraphConnection]::new(
+        $Permissions,
+        $GraphUri,
+        $LoginUri,
+        $Version,
+        $AppId)
+
+    if ( ! $NoConnect.IsPresent ) {
+        $connection.Connect()
+    }
+
+    $connection
 }
